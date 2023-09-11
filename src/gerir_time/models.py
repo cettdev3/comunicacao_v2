f67a733227c98ec7b1f94005bcc458600e93785b
cettdev3 <desenvolvedor3@cett.org.br>
2023-09-11 16:55:04 -0300

    Atualizações do departamento

--- src/gerir_time/models.py
+++ src/gerir_time/models.py
@@ -2,12 +2,20 @@ from django.db import models
 from solicitacoes.models import Escolas
 from django.contrib.auth.models import User
 
+class Departamentos(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    departamento = models.TextField()
+    class  Meta:
+        db_table = 'departamentos'
+
 # Create your models here.
 class Permissoes(models.Model):
+    
     id = models.AutoField(primary_key=True)
     permissao = models.TextField(default='')
     unidade = models.ForeignKey(Escolas,on_delete=models.CASCADE)
     usuario = models.ForeignKey(User,on_delete=models.CASCADE)
-    
+    departamento = models.ForeignKey(Departamentos,on_delete=models.CASCADE)
     class  Meta:
         db_table = 'permissoes'
