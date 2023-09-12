b4dd37ee2a23059b9c4ed4c8a0a2060af8eccec7
cettdev3 <desenvolvedor3@cett.org.br>
2023-09-12 13:45:34 -0300

    Atualização para entregar vários arquivos nas tarefas

--- src/backlog/views.py
+++ src/backlog/views.py
@@ -5,6 +5,9 @@ from django.db import transaction
 from django.http import JsonResponse
 import datetime
 from gerir_time.models import Permissoes
+from django.core.files.storage import FileSystemStorage
+import json
+import ast
 # Create your views here.
 @login_required(login_url='/')
 def Backlog(request):
@@ -18,22 +21,37 @@ def Ajax_View_Task(request):
     permissoes = Permissoes.objects.filter(usuario_id=request.user.id).first
     idtask = request.GET['tarefaId']
     tarefa = Tarefas.objects.filter(id=idtask).first()
+    arquivos = tarefa.arquivos
+    arquivos_list = ast.literal_eval(arquivos)
+
     
-    
-    return render(request, 'ajax/ajax_view_task.html', {'tarefa': tarefa,'permissoes':permissoes})
+    return render(request, 'ajax/ajax_view_task.html', {'tarefa': tarefa,'permissoes':permissoes,'arquivos':arquivos_list})
 
 @login_required(login_url='/')
 def Ajax_Move_Task(request):
     try:
         with transaction.atomic():
-
+            print(request.POST)
             idtask = request.POST['taskId']
             step = request.POST['step']
             entrega = request.POST.get('entrega')
+
+            try:
+                arquivos_task = []
+                arquivos = request.FILES.getlist('files[]')
+                for arquivo in arquivos:
+                    fs1 = FileSystemStorage()
+                    filename1 = fs1.save(arquivo.name, arquivo)
+                    arquivo_url = fs1.url(filename1)
+                    arquivos_task.append(arquivo_url)
+            except:
+                arquivo_url = ''
+
             tarefa = Tarefas.objects.get(id=idtask)
             if entrega:
                 tarefa.descricao_entrega = entrega
                 tarefa.data_entrega = datetime.date.today()
+                tarefa.arquivos = arquivos_task
             tarefa.status  = step
 
             tarefa.save()
