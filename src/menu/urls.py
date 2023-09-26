47a7c72d15cc863c150cc4d8ac5a6a58cbcb90db
cettdev3 <desenvolvedor3@cett.org.br>
2023-09-26 13:49:06 -0300

    Atualizações gerais

--- src/menu/urls.py
+++ src/menu/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from .views import Notifications,Notificacoes_User,Get_Notifications,Read_Notify
 
 urlpatterns = [
-   
+    path('busca-notificacao',  Notifications),
+    path('notificacoes',  Notificacoes_User),
+    path('get-notifications',  Get_Notifications),
+    path('notify-read',  Read_Notify),
 ]
--- src/menu/views.py
+++ src/menu/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from gerir_time.models import Permissoes
+from .models import Notificacoes
+from django.http import JsonResponse
 import requests
 
 @login_required(login_url='/')
@@ -10,3 +12,29 @@ def Permissoes_Menu(request):
     print(permissoes)
 
     return {'permissoes':permissoes}
+
+@login_required(login_url='/')
+def Notifications(request):
+    notificacoes = Notificacoes.objects.filter(user_id = request.user.id,readonly = 1).count()
+    data = {'notificacoes': notificacoes}
+    return JsonResponse(data)
+
+@login_required(login_url='/')
+def Notificacoes_User(request):
+    notificacoes = Notificacoes.objects.filter(user_id = request.user.id).all()
+    permissoes = Permissoes.objects.filter(usuario_id=request.user.id).first()
+    return render(request, 'notificacoes.html',{'notificacoes':notificacoes,'permissoes':permissoes})
+
+@login_required(login_url='/')
+def Get_Notifications(request):
+    idNotify = request.GET['id']
+    notificacao = Notificacoes.objects.filter(id=idNotify).first()
+    # permissoes = Permissoes.objects.filter(usuario_id=request.user.id).first()
+    return render(request, 'ajax/modal_notifications.html',{'notificacao':notificacao})
+
+@login_required(login_url='/')
+def Read_Notify(request):
+    idNotify = request.GET['id']
+    notificacao = Notificacoes.objects.filter(id=idNotify).first()
+    # permissoes = Permissoes.objects.filter(usuario_id=request.user.id).first()
+    return render(request, 'ajax/modal_notifications.html',{'notificacao':notificacao})
